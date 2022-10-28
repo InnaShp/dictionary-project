38d59aba9c2977c4aa1e2cc6a19bc84221792613
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-28 05:43:29 -0700

    Add a search form

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`The keyword is ${keyword}`);
+  }
+
+  function updateKeyword(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="Dictionary">
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Search" onChange={updateKeyword} />
+      </form>
     </div>
   );
 }
