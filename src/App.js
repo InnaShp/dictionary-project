e5a046b087efbbfd53d455571b053f42dc3ec982
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-10-28 05:36:31 -0700

    Create a new component "Dictionary"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React from "react";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      Hello World!
+      <h1>Dictionary</h1>
+      <Dictionary />
+      <footer>Coded by Inna Shpachynska</footer>
     </div>
   );
 }
