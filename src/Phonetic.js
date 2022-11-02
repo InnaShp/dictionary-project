3a91e1099d4120f29486173fbaf947a01d7137a7
InnaShp <107990414+InnaShp@users.noreply.github.com>
2022-11-02 13:03:14 -0700

    Import Howler to play the word pronunciation

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { Howl } from "howler";
 
 export default function Phonetic(props) {
+  const soundSrc = `${props.phonetic.audio}`;
+
+  function callSound(src) {
+    const sound = new Howl({
+      src,
+      html5: true,
+    });
+    sound.play();
+  }
+
+
+
   return (
     <div className="Phonetic">
-      <a href={props.phonetic.audio} rel="noreferrer" target="_blank">
-        Listen
-      </a>
-      <br />
+        
+        <button onClick={() => callSound(soundSrc)}>
+          
+          Listen
+        </button>
+        
       {props.phonetic.text}
     </div>
   );
+  
+  
+ 
+  
+  
 }
